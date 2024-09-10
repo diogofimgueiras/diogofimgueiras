80c594754ee251f35b5a3d72ba370030c8eb4f21
diogofimgueiras <diogoleoneldias44@gmail.com>
2024-09-10 08:47:57 -0300

    ooutros

---
+++ aula.py
@@ -0,0 +1,9 @@
+#entradas
+ano_atual = 2024
+ano_nascimento = input("QUAL O ANO QUE VOCẼ NASCEU?")
+
+#processamento
+idade = ano_atual - int(ano_nascimento)
+
+#Saída
+print("vocẽ tem "+str(idade)+" anos")
---
+++ jokepo.py
@@ -0,0 +1,18 @@
+import random
+
+jogada= input("ESCOLHA UMA OPCAO:[ 0 - PEDRA | 1 - PAPEL | 2 - TESOURA]") 
+
+cpu = random.randint(0,2)
+
+#Play ganha?
+ 
+if jogada == 0 && cpu == 2 print(cpu)
+print("play venceu")
+
+#CPU GANHAR
+if jogada == 0 and cpu == 1:
+    print ("CPU Venceu")
+if jogada == 1 and cpu == 2:
+    print("play venceu")
+if jogada == 2 and cpu == print(cpu)1
+    
